53419a53424b78b7f0752417384da7917ac73c2b
douglas <douglas@gmail.com>
2020-03-14 19:40:16 -0300

    estrutura-base

---
+++ src/test/java/com/souza/librayapi/LibrayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souza.librayapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
