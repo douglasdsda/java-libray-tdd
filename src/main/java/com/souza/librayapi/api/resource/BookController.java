70f0f4caed4cfd736e45a318a9a3ad7ed00645a2
douglas <douglas@gmail.com>
2020-03-15 14:21:24 -0300

    test created

--- src/main/java/com/souza/librayapi/api/resource/BookController.java
+++ src/main/java/com/souza/librayapi/api/resource/BookController.java
@@ -3,7 +3,9 @@ package com.souza.librayapi.api.resource;
 import com.souza.librayapi.api.dto.BookDTO;
 import com.souza.librayapi.api.model.Book.Book;
 import com.souza.librayapi.service.BookService;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,26 +14,24 @@ public class BookController {
 
 
     private BookService service;
+    private ModelMapper modelMapper;
 
-    public BookController(BookService service){
+    public BookController(BookService service, ModelMapper modelMapper){
+
         this.service = service;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public BookDTO create(@RequestBody BookDTO dto){
-        Book entity = Book.builder()
-                .author(dto.getAuthor())
-                .title(dto.getTitle())
-                .isbn(dto.getIsbn())
-                .build();
+        Book entity = modelMapper.map(dto, Book.class);
+              //  Book.builder()
+               // .author(dto.getAuthor())
+               // .title(dto.getTitle())
+               // .isbn(dto.getIsbn())
+              //  .build();
         entity = service.save(entity);
-        return BookDTO.builder()
-                .id(entity.getId())
-                .author(entity.getAuthor())
-                .title(entity.getTitle())
-                .isbn(entity.getIsbn())
-                .build()
-                ;
+        return modelMapper.map(entity, BookDTO.class);
     }
 }
---
+++ src/test/java/com/souza/librayapi/service/BookServiceTest.java
@@ -0,0 +1,5 @@
+package com.souza.librayapi.service;
+
+public class BookServiceTest {
+
+}
